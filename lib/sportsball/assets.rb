0419489d084d64a061067af869fad4ae19b2be29
Mike Silvis <mikesilvis@gmail.com>
2015-03-29 17:03:50 -0700

    initial commit of the sportsball asset

---
+++ lib/sportsball/assets.rb
@@ -0,0 +1,25 @@
+require "sportsball/assets/version"
+
+module Sportsball
+  module Assets
+    ASSET_STRINGS = [
+      'ASSET_STRING_1',
+      'ASSET_STRING_2'
+    ]
+    ASSET_HASHES = ASSET_STRINGS.map do |asset_string|
+      Digest::MD5.hexdigest(asset_string)
+    end
+
+    def self.encode_path(path)
+      "#{path}-#{ASSET_HASHES.last}.png"
+    end
+
+    def self.decode_path(path)
+      path.tap do |updated_path|
+        ASSET_HASHES.each do |asset_hash|
+          updated_path.gsub!("-#{asset_hash}", '')
+        end
+      end
+    end
+  end
+end
